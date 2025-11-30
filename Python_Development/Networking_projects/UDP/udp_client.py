f43700b0eff5034a949f35efdfc5fab1414448f5
Balkar Sidhu <148617889+Balkar13@users.noreply.github.com>
2025-11-30 05:10:16 +0000

    Update udp_client.py

--- Python_Development/Networking_projects/UDP/udp_client.py
+++ Python_Development/Networking_projects/UDP/udp_client.py
@@ -1,1 +1,16 @@
+import socket
 
+target_host = "127.0.0.1"
+target_port = 9997
+#create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#send some data
+input1 = input("enter the data: ")
+input2 = input1.encode()
+client.sendto((input2) ,(target_host,target_port))
+
+#receive some data
+data,addr = client.recvfrom(4096)
+print(data.decode())
+client.close()
